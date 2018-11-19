8e8d1e0073c73d9f9d72eace0870e23698cb3b41
TBExtent <jedpreist@gmail.com>
2018-11-19 16:49:27 +0000

    jumping

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -6,8 +6,11 @@ public class PlayerControls : MonoBehaviour {
 
 	Rigidbody rigidBody;
 	Transform cameraTransform;
+	
 	float speed = 5f;
 	float sensitivity = 5f;
+	float jumpHeight = 10f;
+	float groundDist = 0.5f;
 	bool invertX = false;
 	bool invertY = true;
 
@@ -21,7 +24,7 @@ public class PlayerControls : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+ 		float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
 		float rotationY = cameraTransform.localEulerAngles.x + Input.GetAxis("Mouse Y") * sensitivity;
 
 		if (invertX) rotationX = transform.localEulerAngles.y - Input.GetAxis("Mouse X") * sensitivity;
@@ -34,5 +37,11 @@ public class PlayerControls : MonoBehaviour {
 		cameraTransform.localEulerAngles = new Vector3(rotationY, cameraTransform.localEulerAngles.y, cameraTransform.localEulerAngles.z);
 
 		rigidBody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed * Mathf.Cos(rotationX * Mathf.Deg2Rad) + Input.GetAxis("Vertical") * speed * Mathf.Sin(rotationX * Mathf.Deg2Rad), rigidBody.velocity.y, Input.GetAxis("Vertical") * speed * Mathf.Cos(rotationX * Mathf.Deg2Rad) - Input.GetAxis("Horizontal") * speed * Mathf.Sin(rotationX * Mathf.Deg2Rad));
+		Vector3 floorPosition = new Vector3(transform.position.x, transform.position.y - 0.9f, transform.position.z);
+		if (Input.GetKey(KeyCode.Space) && Physics.Raycast(floorPosition, Vector3.down, groundDist) && Mathf.Abs(rigidBody.velocity.y) < 5) {
+			//Debug.Log("aaa");
+			rigidBody.velocity = new Vector3(rigidBody.velocity.x, jumpHeight, rigidBody.velocity.z);
+		}
+		Debug.Log(rigidBody.velocity.y);
 	}
 }
