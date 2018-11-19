ed71fd50a1d585a7517af38af654ffea65dc3001
TBExtent <jedpreist@gmail.com>
2018-11-19 19:54:33 +0000

    fuck friction

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -7,13 +7,13 @@ public class PlayerControls : MonoBehaviour {
 	Rigidbody rigidBody;
 	Transform cameraTransform;
 	
-	float speed = 5f;
-	float sensitivity = 5f;
-	float jumpHeight = 7f;
-	float bounceHeight = 20f;
+	public float speed = 6f;
+	public float sensitivity = 5f;
+	public float jumpHeight = 7f;
+	public float bounceHeight = 20f;
 	float groundDist = 0.5f;
-	bool invertX = false;
-	bool invertY = true;
+	public bool invertX = false;
+	public  bool invertY = true;
 
 	// Use this for initialization
 	void Start () {
@@ -37,7 +37,9 @@ public class PlayerControls : MonoBehaviour {
 		transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, rotationX, transform.localEulerAngles.z);
 		cameraTransform.localEulerAngles = new Vector3(rotationY, cameraTransform.localEulerAngles.y, cameraTransform.localEulerAngles.z);
 
+		//if (!Physics.Raycast(transform.position, new Vector3(Input.GetAxis("Horizontal") * Mathf.Cos(rotationX * Mathf.Deg2Rad) + Input.GetAxis("Vertical") * Mathf.Sin(rotationX * Mathf.Deg2Rad), rigidBody.velocity.y, Input.GetAxis("Vertical") * speed * Mathf.Cos(rotationX * Mathf.Deg2Rad) - Input.GetAxis("Horizontal") * speed * Mathf.Sin(rotationX * Mathf.Deg2Rad)), 1f)) {
 		rigidBody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed * Mathf.Cos(rotationX * Mathf.Deg2Rad) + Input.GetAxis("Vertical") * speed * Mathf.Sin(rotationX * Mathf.Deg2Rad), rigidBody.velocity.y, Input.GetAxis("Vertical") * speed * Mathf.Cos(rotationX * Mathf.Deg2Rad) - Input.GetAxis("Horizontal") * speed * Mathf.Sin(rotationX * Mathf.Deg2Rad));
+		//}
 		Vector3 floorPosition = new Vector3(transform.position.x, transform.position.y - 0.9f, transform.position.z);
 		RaycastHit hit;
 		bool onGround = Physics.Raycast(floorPosition, Vector3.down, out hit, groundDist);
@@ -45,9 +47,7 @@ public class PlayerControls : MonoBehaviour {
 			rigidBody.velocity = new Vector3(rigidBody.velocity.x, bounceHeight, rigidBody.velocity.z);
 		}
 		else if (Input.GetKey(KeyCode.Space) && onGround && Mathf.Abs(rigidBody.velocity.y) < 5) {
-			//Debug.Log("aaa");
 			rigidBody.velocity = new Vector3(rigidBody.velocity.x, jumpHeight, rigidBody.velocity.z);
 		}
-		Debug.Log(rigidBody.velocity.y);
 	}
 }
