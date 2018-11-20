9b8f84a6ad79b482e91c2cd6f9894f938db0c1d5
CynicalCheddar <nadroj.taylor99@gmail.com>
2018-11-20 10:49:38 +0000

    Blargfest

---
+++ Assets/Online Scripts/HostDisconnect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HostDisconnect : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMasterClientSwitched()
+    {
+        returnToMenu();
+
+    }
+    void returnToMenu()
+    {
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
